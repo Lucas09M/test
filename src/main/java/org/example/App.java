d692a7ff805d9bcf189186acd4eb2e6b2da25185
Lucas09M <lucas.miralles09@gmail.com>
2023-01-09 13:44:45 +0100

    test commit

---
+++ src/main/java/org/example/App.java
@@ -0,0 +1,14 @@
+package org.example;
+
+/**
+ * Hello world!
+ *
+ */
+// TODO : Faire un test !
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
